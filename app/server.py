3392a79faa425c5729b23f581b817781237b388f
PythonBen <32574794+PythonBen@users.noreply.github.com>
2019-12-02 15:37:49 +0100

    pour_champignons_app
    Modifications for mushrooms app

--- app/server.py
+++ app/server.py
@@ -9,10 +9,13 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1weMuTJzzqYtaHolm7Mpxo45o7Ewg0iPA'
+#export_file_name = 'export.pkl'
+export_file_name = 'good_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'grizzly', 'teddys']
+classes = ['cepes', 'girolles', 'trompettes', 'chanterelles', 'sanguins','oronges','pied_de_moutons']
 path = Path(__file__).parent
 
 app = Starlette()
